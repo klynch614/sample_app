4b48275c1824f45241b4397de30801e050da42a5
Katie <klynch614@gmail.com>
2013-01-01 19:55:43 -0700

    Sign in function (w/ test failures)

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ SampleApp::Application.routes.draw do
   
   match '/signup', to: 'users#new'
   
+  match '/signin', to: 'sessions#new'
+  
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
 
 
   # The priority is based upon order of creation:
