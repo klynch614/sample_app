cd8b51b1a46a3a07788d0b822659a3a2698308de
Katie <klynch614@gmail.com>
2013-01-08 12:23:39 -0700

    add user microposts (weird nil error

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -42,6 +42,9 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+        
+        it { should_not have_link('Profile', href:user_path(user)) }
+        it { should_not have_link('Settings', href: edit_user_path(user)) }
       end    
     end
   end
@@ -53,10 +56,8 @@ describe "Authentication" do
       
       describe "when attempting to visit a protected page" do
         before do
+          sign_in(user)
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
         end
         
         describe "after signing in" do
@@ -82,6 +83,18 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+      
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) } 
         end
       end
     end
